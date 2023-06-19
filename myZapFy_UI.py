a5a9ee6d0f87f62bc87894258923d2fc06a1b3ac
mihainuan <mihainuan@gmail.com>
2023-06-19 17:16:28 -0300

    implemented bot in main screen

--- myZapFy_UI.py
+++ myZapFy_UI.py
@@ -29,6 +29,66 @@ msg_box = api[5].strip()
 msg_client = api[6].strip()
 ##--##
 
+client_q = 'pergunta ao cliente:'
+client_a = 'Por favor, responda a mensagem do cliente com base no próximo texto'
+client_copy = 'Os clientes que comprarem nesse mês de junho só paga a primeira Mensal Em agosto, aproveitem a promoção, agende uma visita 719 8451 5414'
+question = client_q + client_a + client_copy
+
+def myZapFy():
+    try:
+        # 1) Getting Notifications and Double-Clicking in notified contact
+        green_icon_unread = driver.find_element(By.CLASS_NAME, ball_notification)
+        green_icon_unread = driver.find_elements(By.CLASS_NAME, ball_notification)
+        
+        # 1.1) Points into the last notification
+        last_icon_unread = green_icon_unread[-1]
+        action_unread = webdriver.common.action_chains.ActionChains(driver)
+        action_unread.move_to_element_with_offset(last_icon_unread, 0, -20)
+        
+        # 1.2) Double-click it
+        action_unread.click()
+        action_unread.perform()
+        action_unread.click()
+        action_unread.perform()
+
+        # 2) Messages Received
+        all_msgs = driver.find_elements(By.CLASS_NAME, msg_client)
+        all_msgs_txt = [e.text for e in all_msgs]
+        last_msg = all_msgs_txt[-1] # Gets Last message
+        print(last_msg)
+        
+        # 3) Process Messages with API (ChatGPT)
+        openai.api_key = openaiapi
+        response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=last_msg,
+        temperature=1,
+        max_tokens=256,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+        )
+        answer = response['choices'][0]['text']
+        print(answer)
+        time.sleep(3)
+
+        # 4) Replies Message to WhatsApp
+        text_field = driver.find_element(By.XPATH, msg_box)
+        text_field.click()
+        time.sleep(3)
+        text_field.send_keys(answer, Keys.ENTER)
+
+        # 5) Close the conversation with the contact
+        time.sleep(2)
+        webdriver.ActionChains(driver).send_keys(Keys.ESCAPE).perform()
+
+        print("New notification:\n")
+        print(last_msg)
+        sleep(3) # three seconds
+
+    except:
+        print("Waiting for notifications...")
+
 # First screen (password login)
 screen1 = [
     [sg.Text('YOUR PASSWORD')],
@@ -40,7 +100,7 @@ screen1 = [
 screen2 = [
     [sg.Text('Welcome to MyZapFy!')],
     [sg.Text('Please enter your OpenAI API Key')],
-    [sg.Input(key='OpenAI API Key')],
+    [sg.Input(key='openaiapi')],
     [sg.Multiline(size=(80,20), key='text')],
     [sg.Text('Please have your WhatsApp Online')],
     [sg.Text('Click below to capture WhatsApp QR Code')],
@@ -58,4 +118,16 @@ while True:
         senha = values['password']
         if senha == token3:
             window1.close()
-            event, values = window2.read()
+            event2, values2 = window2.read()
+            if event2 == 'Capture QR Code!':
+                window2.close()
+                dir_path = os.getcwd()
+                openaiapi = values2['openaiapi']
+                text = values2['text']
+                opt = Options()
+                opt.add_argument(r"user-data-dir=" + dir_path + "profile/zap")
+                driver = webdriver.Chrome(options=opt)
+                driver.get("https://web.whatsapp.com/")
+                time.sleep(5)
+                while True:
+                    myZapFy()
